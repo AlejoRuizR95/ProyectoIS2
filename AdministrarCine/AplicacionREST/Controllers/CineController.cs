fc8ddec8977ac6415e20c29f831987968db55fbd
AlejoRuizR95 <65086756+AlejoRuizR95@users.noreply.github.com>
2020-05-30 18:22:14 -0500

    Cors

--- AdministrarCine/AplicacionREST/Controllers/CineController.cs
+++ AdministrarCine/AplicacionREST/Controllers/CineController.cs
@@ -5,37 +5,36 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using LogicaNegocio;
+using System.Web.Http.Cors;
 
 namespace AplicacionREST.Controllers
 {
+    [EnableCors("*", "*", "*")]
+    
     public class CineController : ApiController
     {
-        ClsSala S = new ClsSala();
-
+       
+        
         // GET api/<controller>/5
         public string Get(string Columna, int Fila, int Dato, string Tabla)
         {
+            ClsSala S = new ClsSala();
             S.Columna = Columna;
             S.Fila = Fila;
             S.Dato = Dato;
-            S.Tabla = Tabla;
+            S.Tabla = Tabla;            
             return S.Registrar_Silla();
         }
 
+        // GET api/<controller>/5
         public List<string> Get()
         {
+            ClsSala S = new ClsSala();
             return S.ListadoPeliculas();
         }
 
-        public List<string> Get(string pelicula)
-        {
-            return S.LeerFechas(pelicula);
-        }
+        
 
-        public List<string> Get(string pelicula, string Dia)
-        {
-            return S.LeerHoras(pelicula, Dia);
-        }
 
 
     }
